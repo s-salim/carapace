7a6b731093d99c17087be7ca8c2f1f8936f17edf
Soufian <ssa@yujia.fr>
2013-03-28 17:04:09 +0100

    test skeleton classes

---
+++ tests/lib/Carapace/Core/GUI/FrameTest.php
@@ -0,0 +1,20 @@
+<?php
+
+/*
+ * This file is part of the Carapace package.
+ *
+ * (c) Soufian Salim <soufi@nsal.im>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Carapace\Core\GUI;
+
+/**
+ * @author Soufian Salim <soufi@nsal.im>
+ */
+class FrameTest extends \PHPUnit_Framework_TestCase
+{
+
+}
---
+++ tests/lib/Carapace/Core/Log/HandlerTest.php
@@ -0,0 +1,20 @@
+<?php
+
+/*
+ * This file is part of the Carapace package.
+ *
+ * (c) Soufian Salim <soufi@nsal.im>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Carapace\Core\Log;
+
+/**
+ * @author Soufian Salim <soufi@nsal.im>
+ */
+class HandlerTest extends \PHPUnit_Framework_TestCase
+{
+
+}
